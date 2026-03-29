574a90012aa2782a371d79fefe58150417394f6b
Ramakrishna-19 <ramakrishnadyavari1920@gmail.com>
2026-03-29 17:59:09 +0530

    key check

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -31,7 +31,9 @@ const interviewReportSchema = z.object({
 
 async function generateInterviewReport({ resume, selfDescription, jobDescription }) {
     console.log("KEY:", process.env.OPENROUTER_API_KEY);
-    
+    console.log("ENV KEYS:", object.keys(process.env));
+    console.log("OPENROUTER:", process.env.OPENROUTER_API_KEY);
+
     const prompt = `
 Generate a professional interview report in STRICT JSON format.
 
